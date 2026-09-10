1b50b3fcfaa952627e05344ae804788c51e74efa
projecthyper10-stack <projecthyper1.0@gmail.com>
2026-09-10 18:41:22 +0700

    Update Games.lua

--- UI.main/Games.lua
+++ UI.main/Games.lua
@@ -4,55 +4,43 @@ return {
         RequiresKey = false, 
         PlaceIds = { 2753915549, 4442272183, 7449423635 },
         Local = "Scripts/M.lua/BF V1",
-        Remote = "https://raw.githubusercontent.com/projectsingularityv1-debug/HYPER-MAIN/refs/heads/main/M.lua/BF%20V1"
+        Remote = "http://raw.githubusercontent.com/projecthyper10-stack/HYPER-LOADER/refs/heads/main/M.lua/BF%20V1"
     },
     ["Murder Mystery 2"] = {
         RequiresKey = false, 
         PlaceIds = { 142823291 },
         Local = "Scripts/M.lua/MM2 DONE.lua",
-        Remote = "https://raw.githubusercontent.com/projectsingularityv1-debug/HYPER-MAIN/refs/heads/main/M.lua/MM2%20DONE.lua"
-    },
-    ["The Walking Dead"] = {
-        RequiresKey = false, 
-        PlaceIds = { 128039018996175 },
-        Local = "Scripts/M.lua/TWD V1.lua",
-        Remote = "https://raw.githubusercontent.com/projectsingularityv1-debug/HYPER-MAIN/refs/heads/main/M.lua/TWD%20V1.lua"
-    },
-    ["Fisch"] = {
-        RequiresKey = false, 
-        PlaceIds = { 16732694052 },
-        Local = "Scripts/M.lua/LK AUTO.lua",
-        Remote = "https://raw.githubusercontent.com/projectsingularityv1-debug/HYPER-MAIN/refs/heads/main/M.lua/LK%20AUTO.lua"
+        Remote = "https://github.com/projecthyper10-stack/HYPER-LOADER/raw/refs/heads/main/M.lua/MM2%20DONE.lua"
     },
     ["Mine a Mountain"] = {
         RequiresKey = false, 
         PlaceIds = { 125927821145949 },
         Local = "Scripts/M.lua/fame 222.lua",
-        Remote = "https://raw.githubusercontent.com/projectsingularityv1-debug/HYPER-MAIN/refs/heads/main/M.lua/fame%20222.lua"
+        Remote = "https://raw.githubusercontent.com/projecthyper10-stack/HYPER-LOADER/refs/heads/main/M.lua/fame%20222.lua"
     },
     ["Laundry Simulator"] = {
         RequiresKey = false, 
         PlaceIds = { 6305942109 },
         Local = "Scripts/M.lua/LaundrySimulator_AutoFarm.lua",
-        Remote = "https://raw.githubusercontent.com/projectsingularityv1-debug/HYPER-MAIN/refs/heads/main/M.lua/LaundrySimulator_AutoFarm.lua"
+        Remote = "https://raw.githubusercontent.com/projecthyper10-stack/HYPER-LOADER/refs/heads/main/M.lua/LaundrySimulator_AutoFarm.lua"
     },
     ["Cali Shootout"] = {
         RequiresKey = false, 
         PlaceIds = { 12077443856 },
         Local = "Scripts/M.lua/gun auto.lua",
-        Remote = "https://raw.githubusercontent.com/projectsingularityv1-debug/HYPER-MAIN/refs/heads/main/M.lua/gun%20auto.lua"
+        Remote = "https://raw.githubusercontent.com/projecthyper10-stack/HYPER-LOADER/refs/heads/main/M.lua/gun%20auto.lua"
     },
     ["Basketball"] = {
         RequiresKey = false, 
         PlaceIds = { 16033173781, 16270425785 },
-        Local = "Scripts/Basketball_XINZ.lua",
-        Remote = "https://raw.githubusercontent.com/projectsingularityv1-debug/HYPER-MAIN/refs/heads/main/M.lua/Basketball_XINZ.lua"
+        Local = "Scripts/Basketball_HYPER.lua",
+        Remote = "https://raw.githubusercontent.com/projectsingularityv1-debug/HYPER-MAIN/refs/heads/main/M.lua/Basketball_HYPER.lua"
     },
     ["BasketballZero"] = {
         RequiresKey = false, 
         PlaceIds = { 129230994638464,130739873848552},
-        Local = "Scripts/Basketball_XINZ.lua",
-        Remote = "https://projectsingularity.online/raw/repos/191c9695-c9f9-4b5f-805f-d87e8e3b8fac/Basketball_XINZ.lua"
+        Local = "Scripts/Basketball_HYPER.lua",
+        Remote = "https://raw.githubusercontent.com/projecthyper10-stack/HYPER-LOADER/refs/heads/main/M.lua/Basketball_XINZ.lua"
     },
     ["Universal"] = {
         RequiresKey = false,
